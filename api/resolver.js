26546678046e0e09026cce856c1b675c16b7c882
Mircea <mircea@hickery.net>
2017-10-08 10:32:48 +0000

    code alignment

--- api/resolver.js
+++ api/resolver.js
@@ -1,13 +1,11 @@
 var request = require('request');
 var _ = require('underscore');
 
-
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
-  var term = req.query.text.trim();
-  res.json({
-    body: term,
-    raw: true
-  });
-  return;
+    var term = req.query.text.trim();
+    res.json({
+        body: term,
+        raw: true
+    });
 };
--- api/typeahead.js
+++ api/typeahead.js
@@ -1,48 +1,46 @@
 var request = require('request');
 var _ = require('underscore');
 
-
 // The Type Ahead API.
 module.exports = function(req, res) {
-  var term = req.query.text.trim();
-  if (!term) {
-    res.json([{
-      title: '<i>(enter a search term)</i>',
-      text: ''
-    }]);
-    return;
-  }
-  request({
-    url: 'https://api.datamuse.com/words',
-    qs: {
-        rel_nry: term,
-        max: 15
-    },
-    gzip: true,
-    json: true,
-    timeout: 10 * 1000
-  }, function(err, response) {
-   	if (err || response.statusCode !== 200 || !response.body || !response.body) {
-      res.status(500).send('Error');
-      return;
-    }  
-    var results = _.chain(response.body)
-        .map(function(word) {
-            return {
-                title: word.word,
-                text: word.word
-            };
-        })
-        .value();
+    var term = req.query.text.trim();
+    if (!term) {
+        res.json([{
+            title: '<i>(enter a search term)</i>',
+            text: ''
+        }]);
+        return;
+    }
     
-   	if (results.length === 0) {
-      res.json([{
-        title: '<i>(no results)</i>',
-        text: ''
-      }]);
-    } else {
-      res.json(results);
-    } 
-  });
-    
+    request({
+        url: 'https://api.datamuse.com/words',
+        qs: {
+            rel_nry: term,
+            max: 15
+        },
+        gzip: true,
+        json: true,
+        timeout: 10 * 1000
+        }, function(err, response) {
+            if (err || response.statusCode !== 200 || !response.body) {
+                res.status(500).send('Error');
+                return;
+            }
+            var results = _.chain(response.body)
+                .map(function(word) {
+                    return {
+                        title: word.word,
+                        text: word.word
+                    };
+                })
+                .value();
+            if (results.length === 0) {
+                res.json([{
+                    title: '<i>(no results)</i>',
+                    text: ''
+                }]);
+            } else {
+                res.json(results);
+            }
+        });
 };
